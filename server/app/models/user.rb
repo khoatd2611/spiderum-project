cddd233df2d7334179e985ce337c6387b83c4acf
fateskink <shaynenguyen9@gmail.com>
2022-09-25 18:53:25 +0700

    create remember_token

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # :registerable,:recoverable, :rememberable, :validatable
+  attr_accessor :remember_token
   devise :database_authenticatable
   has_many :posts
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end

4a686cd1ef8a670e0b48cc521539aa74014b2a35
fateskink <shaynenguyen9@gmail.com>
2022-09-26 01:27:22 +0700

    account activation >> edit

--- server/app/controllers/api/v1/user1/account_activations_controller.rb
+++ server/app/controllers/api/v1/user1/account_activations_controller.rb
@@ -3,7 +3,14 @@ module Api
     module User1
       class AccountActivationsController < ApplicationController
         def edit
-        
+          user = User.find_by(email: params[:email])
+          if user && !user.activated? && user.authenticated?(:activation, params[:id])
+            user.update_attribute(:activated, true)
+            user.update_attribute(:activated_at, Time.zone.now)
+            render json: {message: "Account activated!", user: user}, status: :ok
+          else
+            render json: {message: "Invalid activation link"}, status: :unprocessable_entity
+          end
         end
       end
     end
